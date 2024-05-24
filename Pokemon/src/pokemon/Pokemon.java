9d6cb3ce6d3f3482d1fc34c5bba1b039ab480a84
https://github.com/CristianJl2001/2670586-ADSO.git <https://github.com/CristianJl2001/2670586-ADSO.git>
2024-05-24 11:21:12 -0500

    peque?os_avances

---
+++ Pokemon/src/pokemon/Pokemon.java
@@ -0,0 +1,15 @@
+
+package pokemon;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import pokemon.Utils.ConsumoAPI;
+public class Pokemon {
+
+
+    public static void main(String[] args) {
+        Ventana venatanaPokemon = new Ventana();
+    }
+    
+}
